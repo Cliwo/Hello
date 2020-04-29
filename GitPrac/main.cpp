450a4f62a988ea92528146cb6cb06e752d479cf6
Chan <cliwo@naver.com>
2020-04-29 16:38:12 +0900

    add Hello!

---
+++ GitPrac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitPrac
+//
+//  Created by SEUNGCHAN on 2020/04/29.
+//  Copyright © 2020 SEUNGCHAN. All rights reserved.
+//
+
+#include "stdio.h"
+
+int main()
+{
+    printf("hello!\n");
+    return 0;
+}
