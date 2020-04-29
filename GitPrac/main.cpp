2bed3fcc6cd9fb2071eea8b3974d901a7f90cbe2
Chan <cliwo@naver.com>
2020-04-29 16:46:39 +0900

    니나이몇살이노 함수 추가

--- GitPrac/main.cpp
+++ GitPrac/main.cpp
@@ -7,9 +7,20 @@
 //
 
 #include "stdio.h"
+void name_out(int var)
+{
+    printf("내 나이는 %d\n",var);
+    return;
+}
 
 int main()
 {
+    int var;
     printf("hello!\n");
+    printf("니 나이 몇살이노?\n");
+    scanf("%d",&var);
+    name_out(var);
     return 0;
 }
+
+
